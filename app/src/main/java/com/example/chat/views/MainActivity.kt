47b4bd32050f793eab4e248f5831e50b437c6d5d
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-03-23 08:47:12 +0100

    done refactor

--- app/src/main/java/com/example/chat/views/MainActivity.kt
+++ app/src/main/java/com/example/chat/views/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.example.chat.views
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.chat.R
 import com.example.chat.databinding.ActivityMainBinding
 import com.example.chat.repository.UserService
 import utils.ActivityRouting
--- app/src/main/java/com/example/chat/views/MainFragment.kt
+++ app/src/main/java/com/example/chat/views/MainFragment.kt
@@ -1,7 +1,11 @@
 package com.example.chat.views
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -14,6 +18,37 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     private var fragmentSignUpBinding: FragmentMainBinding? = null
     private val viewModel: AuthViewModel by viewModels()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar)
+
+        toolbar.inflateMenu(R.menu.menu)
+        toolbar.setOnMenuItemClickListener {
+            // switch case in kotlin
+            when (it.itemId) {
+                R.id.menu_settings -> {
+                    Toast.makeText(requireActivity(), "Settings", Toast.LENGTH_SHORT).show()
+                }
+                R.id.menu_grup_chat -> {
+                    Toast.makeText(requireActivity(), "Group Chat", Toast.LENGTH_SHORT).show()
+                }
+                R.id.menu_log_out -> {
+                    viewModel.signOut()
+                }
+                else -> { // Default switch case
+                    Toast.makeText(requireActivity(), "Err in selecting option", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+            true
+        }
+
+        return view
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -27,9 +62,6 @@ class MainFragment : Fragment(R.layout.fragment_main) {
                 findNavController().navigate(MainFragmentDirections.actionMainFragmentToSignInFragment())
             }
         }
-
-        binding.btnSignOut.setOnClickListener { viewModel.signOut() }
-
     }
 
     override fun onDestroyView() {
