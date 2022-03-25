45c59b36bcf9752b4cece58f55023d0edda19cce
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-03-25 12:40:44 +0100

    minor fixes and added icon

--- app/src/main/java/com/example/chat/views/MainActivity.kt
+++ app/src/main/java/com/example/chat/views/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.chat.views
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.example.chat.R
 import com.example.chat.databinding.ActivityMainBinding
 import com.example.chat.repository.UserService
 import utils.ActivityRouting
@@ -16,6 +19,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         activityRouting = ActivityRouting(this)
         userService = UserService(this)
+
+        val window = this.window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = this.resources.getColor(R.color.black)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
