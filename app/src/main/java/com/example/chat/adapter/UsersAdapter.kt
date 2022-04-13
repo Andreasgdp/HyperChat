741d235733e68e7ca4a2d36874037b965a92aacf
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-04-13 10:10:12 +0200

    added show last message

--- app/src/main/java/com/example/chat/adapter/UsersAdapter.kt
+++ app/src/main/java/com/example/chat/adapter/UsersAdapter.kt
@@ -9,11 +9,19 @@ import android.widget.TextView
 import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chat.R
+import com.example.chat.models.Message
 import com.example.chat.models.User
 import com.example.chat.views.MainFragmentDirections
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
 
-class UsersAdapter(_list: ArrayList<User>, _context: Context): RecyclerView.Adapter<UsersAdapter.ViewHolder>() {
+class UsersAdapter(_list: ArrayList<User>, _context: Context) :
+    RecyclerView.Adapter<UsersAdapter.ViewHolder>() {
 
     private val list: ArrayList<User> = _list
     private val context: Context = _context
@@ -22,6 +30,7 @@ class UsersAdapter(_list: ArrayList<User>, _context: Context): RecyclerView.Adap
         val image: ImageView = itemView.findViewById(R.id.list_profile_image)
         val userName: TextView = itemView.findViewById(R.id.list_user_name)
         val lastMessage: TextView = itemView.findViewById(R.id.list_last_message)
+        val lastMessageDate: TextView = itemView.findViewById(R.id.list_last_message_date)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -34,8 +43,38 @@ class UsersAdapter(_list: ArrayList<User>, _context: Context): RecyclerView.Adap
         Picasso.get().load(user.profilePic).placeholder((R.drawable.avatar4)).into(holder.image)
         holder.userName.text = user.userName
 
+
+        val database =
+            Firebase.database("https://hyperchat-282b7-default-rtdb.europe-west1.firebasedatabase.app/")
+        val chatsRef = database.getReference("Chats")
+        chatsRef.child(FirebaseAuth.getInstance().uid + user.userId)
+            .orderByChild("timestamp").limitToLast(1)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val children = snapshot.children
+                    var model = Message();
+                    children.forEach {
+                        println(it.toString())
+                        model = it.getValue(Message::class.java)!!;
+                    }
+                    holder.lastMessage.text = model.message
+                    holder.lastMessageDate.text = model.timestamp
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+
         holder.itemView.setOnClickListener { view ->
-            findNavController(view).navigate(MainFragmentDirections.actionMainFragmentToActiveChatFragment(user.userId.toString(), user.userName.toString(), user.profilePic.toString()))
+            findNavController(view).navigate(
+                MainFragmentDirections.actionMainFragmentToActiveChatFragment(
+                    user.userId.toString(),
+                    user.userName.toString(),
+                    user.profilePic.toString()
+                )
+            )
         }
     }
 
